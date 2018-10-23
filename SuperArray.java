a673f8aae732ebb48f87c46412fa38f0cc605693
Jackson Zou <jzou00@stuy.edu>
2018-10-23 11:02:34 -0400

    Created the class, with clear(), size(), isEmpty(), add(), and toString() methods. No testing

---
+++ SuperArray.java
@@ -0,0 +1,44 @@
+public class SuperArray{
+  private String[] data;
+  private int size;
+
+  public SuperArray() {
+    data = new String[10]
+    size = 0;
+  }
+
+  public void clear() {
+    size = 0;
+  }
+
+  public int size() {
+    return size;
+  }
+
+  public boolean isEmpty() {
+    if (size == 0) {
+      return true;
+    }
+    return false;
+  }
+
+  public boolean add(String str) {
+    if (data.length > size) {
+      data[size] = str;
+      size += 1;
+      return true;
+    }
+    return false;
+  }
+
+  public String toString() {
+    if (size > 0) {
+      String f = "[" + data[0];
+      for (int i = 1; i < size; i++) {
+        f += "," + data[i];
+      }
+      f += "]";
+      return f;
+    }
+    return "[]";
+}
